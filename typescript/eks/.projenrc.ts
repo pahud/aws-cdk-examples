2f1a25e1eb1a4c10f82c41b4c5c037b934290390
Pahud Hsieh <pahudnet@gmail.com>
2023-07-20 02:34:44 +0000

    update eks

---
+++ typescript/eks/.projenrc.ts
@@ -0,0 +1,16 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.80.0',
+  defaultReleaseBranch: 'main',
+  name: 'eks',
+  projenrcTs: true,
+  deps: [
+    '@aws-cdk/lambda-layer-kubectl-v26',
+  ]
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
---
+++ typescript/eks/src/main.ts
@@ -0,0 +1,83 @@
+import { App, Stack, StackProps,
+aws_eks as eks,
+aws_iam as iam,
+ } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { getVpc } from './util';
+import { KubectlV26Layer as KubectlLayer } from '@aws-cdk/lambda-layer-kubectl-v26';
+
+export class SingleClusterStack extends Stack {
+  readonly cluster: eks.ICluster;
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    const vpc = getVpc(this);
+    
+    // create a scoped mastersRole
+    const mastersRole = this.createMastersRole();
+
+    this.cluster = new eks.Cluster(this, 'Cluster', {
+      vpc,
+      mastersRole,
+      kubectlLayer: new KubectlLayer(this, 'KubectlLayer'),
+      version: eks.KubernetesVersion.V1_26,
+    });
+
+    // allow mastersRole to access console
+    this.enableConsoleAccess(mastersRole);
+
+  }
+  private createMastersRole(): iam.Role {
+    /**
+     * Define the mastersRole for the cluster and scope down the principals
+     */
+    const mastersRole = new iam.Role(this, 'MastersRole', {
+      // assumedBy: new iam.ArnPrincipal('arn:aws:sts::123456789012:assumed-role/AWSReservedSSO_AdministratorAccess_c4188fabc1dd35a2/pahud'),
+      assumedBy: new iam.ArnPrincipal(Stack.of(this).formatArn({
+        service: 'sts',
+        region: '',
+        resource: 'assumed-role',
+        resourceName: 'AWSReservedSSO_AdministratorAccess_c4188fabc1dd35a2/pahud'
+      }))
+    });
+
+    mastersRole.assumeRolePolicy?.addStatements(new iam.PolicyStatement({
+      actions: ['sts:AssumeRole'],
+      principals: [
+        // arn:aws:sts::123456789012:assumed-role/AWSReservedSSO_AdministratorAccess_c4188fabc1dd35a2/foo
+        new iam.ArnPrincipal(Stack.of(this).formatArn({
+          service: 'sts',
+          region: '',
+          resource: 'assumed-role',
+          resourceName: 'AWSReservedSSO_AdministratorAccess_c4188fabc1dd35a2/foo'
+        })),
+        // arn:aws:sts::123456789012:user/pahud
+        new iam.ArnPrincipal(Stack.of(this).formatArn({
+          service: 'sts',
+          region: '',
+          resource: 'user',
+          resourceName: 'pahud'
+        })),
+      ],
+    }));
+    return mastersRole;
+  }
+  private enableConsoleAccess(role: iam.Role) {
+    role.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        'eks:Describe*',
+        'eks:List*',
+        'eks:AccessKubernetesApi'
+      ],
+      resources: [ this.cluster.clusterArn ],
+    }))
+  } 
+}
+
+const devEnv = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
+
+const app = new App();
+
+new SingleClusterStack(app, 'eks-single-cluster', { env: devEnv });
+
+app.synth();
