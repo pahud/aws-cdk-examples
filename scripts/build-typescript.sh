1005da851310af342605a17d35771063ff7a2968
Pahud Hsieh <pahudnet@gmail.com>
2023-07-24 08:56:19 -0400

    Update build-typescript.sh

--- scripts/build-typescript.sh
+++ scripts/build-typescript.sh
@@ -4,33 +4,33 @@ set -euxo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
 projectname="$1"
 
-
 echo "=============================="
 echo "building project: typescript/$projectname"
 echo "=============================="
 
 cd "typescript/$projectname";
-yarn install --frozen-lockfile
-yarn build
-yarn test
+
+# Check if yarn.lock exists
+if [ -f "yarn.lock" ]; then
+    echo "yarn.lock file found. Running 'yarn install'..."
+    yarn install --frozen-lockfile
+    yarn build
+    yarn test
+# Check if package-lock.json exists
+elif [ -f "package-lock.json" ]; then
+    echo "package-lock.json file found. Running 'npm ci'..."
+    npm ci
+    echo "Running 'npm build'..."
+    npm run build
+    echo "Running 'npm test'..."
+    npm test
+else
+    echo "No lock files found (yarn.lock or package-lock.json). Running 'yarn install'... "
+    yarn install
+    yarn build
+    yarn test
+fi
+
 $scriptdir/synth.sh
 
 exit 0
-
-# # Find and build all NPM projects
-# for pkgJson in $(find typescript -name cdk.json | grep -v node_modules | sort); do
-#     (
-#         echo "=============================="
-#         echo "building project: $(dirname $pkgJson)"
-#         echo "=============================="
-
-#         cd $(dirname $pkgJson)
-#         if [[ -f DO_NOT_AUTOTEST ]]; then exit 0; fi
-
-#         rm -rf package-lock.json node_modules
-#         npm install
-#         npm run build
-
-#         $scriptdir/synth.sh
-#     )
-# done
