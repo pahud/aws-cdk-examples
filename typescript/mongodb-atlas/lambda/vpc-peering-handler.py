441883bd7b07c41d114c41be3177546949e0d813
Pahud Hsieh <pahudnet@gmail.com>
2023-07-31 15:14:38 +0000

    clean up

--- typescript/mongodb-atlas/lambda/vpc-peering-handler.py
+++ typescript/mongodb-atlas/lambda/vpc-peering-handler.py
@@ -22,7 +22,6 @@ def on_update(event):
   physical_id = event["PhysicalResourceId"]
   props = event["ResourceProperties"]
   print("update resource %s with props %s" % (physical_id, props))
-  # ...
   return
 
 def on_delete(event):
@@ -34,23 +33,3 @@ def on_delete(event):
   )
   print(resp)
   return
-  # ...
-  
-# def is_complete(event, context):
-#   physical_id = event["PhysicalResourceId"]
-#   request_type = event["RequestType"]
-#   client = boto3.client('ec2')
-#   resp = client.describe_instance_connect_endpoints(
-#     InstanceConnectEndpointIds=[physical_id]
-#   )
-#   status = resp.get('InstanceConnectEndpoints')[0].get('State')
-
-#   # check if resource is stable based on request_type
-#   if request_type == 'Create':
-#     is_ready = True if status == 'create-complete' else False
-#   if request_type == 'Update':
-#     is_ready = True
-#   if request_type == 'Delete':
-#     is_ready = True if status == 'delete-complete' else False
-  
-#   return { 'IsComplete': is_ready }
